f396462f8ee544af9f5969deae51332d75a37d23
Maximilian Konter <biskit1943@gmx.de>
2018-10-17 13:45:47 +0200

    Merge 21d11054bfbc606d3455a9d43b2d3bb128c95d42

--- app/search/file_search.py
+++ app/search/file_search.py
@@ -1,28 +1,29 @@
-import taglib
-import yaml
 import glob
 import os
 import taglib
 
-class FileSearch():
-    def __init__(self, configFile="./config.yml"):
-        self.configFile = open(configFile)
-        self.configYml = yaml.load(self.configFile)
-        self.formats=self.configYml["Formats"]
+from config import Config
+
+
+class FileSearch:
+    def __init__(self):
+        self.formats = Config.SONG_EXTENSIONS
 
     def search(self, path, *args):
         for fileFormat in self.formats:
             for fileName in glob.iglob((path + "/**/*" + fileFormat), recursive=True):
-                media = {}
-                media["path"] = os.path.dirname(fileName)
-                media["filename"] = os.path.basename(fileName)
+                media = {
+                    "path": os.path.dirname(fileName),
+                    "filename": os.path.basename(fileName)
+                }
                 tags = taglib.File(fileName)
-                media["lenght"] = tags.length
+                media["length"] = tags.length
                 media["meta"] = tags.tags
                 for key in args:
                     if key not in media["meta"]:
                         media["meta"][key] = None
                 yield media
+
 
 if __name__ == "__main__":
     fileSearch = FileSearch()
