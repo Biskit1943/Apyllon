21d11054bfbc606d3455a9d43b2d3bb128c95d42
Maximilian Konter <biskit1943@gmx.de>
2018-10-17 13:45:47 +0200

    Fix config for search

--- config.py
+++ config.py
@@ -4,7 +4,18 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    REQUIRED_META_DATA = ['artist', 'title', 'album', 'genre']
+    REQUIRED_META_DATA = [
+        'artist',
+        'title',
+        'album',
+        'genre',
+    ]
+    SONG_EXTENSIONS = [
+        '.mp3',
+        '.mp4',
+        '.wav',
+    ]
+
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
