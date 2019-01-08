ab8e531c298d99178d7fc8d9381fd6a4347a1ff4
Tim Koehler <tk@perceptor.ai>
2019-01-08 21:44:28 +0100

    change password

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -3,12 +3,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home';
 import {LoginComponent} from './login';
 import {RegisterComponent} from './register';
+import {PasswordComponent} from './password';
 import {AuthGuard} from './_guards';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent, canActivate: [AuthGuard]},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'password', component: PasswordComponent},
 
   // otherwise redirect to home
   {path: '**', redirectTo: ''}
---
+++ frontend/src/app/password/password.component.ts
@@ -0,0 +1,65 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertService, AuthenticationService } from '../_services';
+import { first } from 'rxjs/operators';
+import { User } from '../_models';
+
+@Component({
+  selector: 'app-password',
+  templateUrl: './password.component.html',
+  styleUrls: ['./password.component.css']
+})
+
+export class PasswordComponent implements OnInit {
+  passwordForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  @Input() user: User = null;
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private alertService: AlertService) {
+  }
+
+  ngOnInit() {
+    this.passwordForm = this.formBuilder.group({
+      currentPassword: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    });
+
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.passwordForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.passwordForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.authenticationService.changePassword(this.user.username, this.f.currentPassword, this.f.password.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate([this.returnUrl]);
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
+        });
+  }
+}
