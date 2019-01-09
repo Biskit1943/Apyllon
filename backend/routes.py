304a9fd53c78941371c5735df80206afac054556
Tim Koehler <tk@perceptor.ai>
2019-01-09 15:13:13 +0100

    added route for password change

--- backend/routes.py
+++ backend/routes.py
@@ -40,76 +40,76 @@ def shutdown_session(exception=None):
 # User related endpoints
 app.add_url_rule('/users',
                  view_func=users.UserView.as_view(name='user'),
-                 methods=['GET', 'POST', ]
+                 methods=['GET', 'POST']
                  )
 app.add_url_rule('/users/<int:uid>',
                  view_func=users.UsersIdView.as_view(name='users_id'),
-                 methods=['GET', 'PUT', 'DELETE', ]
+                 methods=['GET', 'PUT', 'DELETE']
                  )
 app.add_url_rule('/users/<string:username>',
                  view_func=users.UsersNameView.as_view(name='users_name'),
-                 methods=['GET', 'PUT', 'DELETE', ]
+                 methods=['GET', 'PUT', 'DELETE']
                  )
 app.add_url_rule('/users/<int:uid>/authenticate',
                  view_func=users.UsersIdAuthView.as_view(name='users_id_auth'),
-                 methods=['POST', ]
+                 methods=['POST']
                  )
 app.add_url_rule('/users/<string:username>/authenticate',
                  view_func=users.UsersNameAuthView.as_view(name='users_name_auth'),
-                 methods=['POST', ]
+                 methods=['POST']
                  )
-app.add_url_rule('/users/<string:username>/authenticate',
+app.add_url_rule('/users/changepw',
                  view_func=users.UsersNameChangePassword.as_view(name='users_change_password'),
-                 methods=['POST', ])
+                 methods=['PUT'])
 
 # Player related endpoints
-app.add_url_rule('/player/play_pause/<string:username>/<string:state>',
-                 view_func=player.PlayerPlayPause.as_view(name='player_play_pause'),
-                 methods=['PUT'],
-                 endpoint='play_pause_username_state'
-                 )
-app.add_url_rule('/player/next/<string:username>',
-                 view_func=player.PlayerNext.as_view(name='player_next'),
-                 methods=['PUT'],
-                 endpoint='next_username'
-                 )
-app.add_url_rule('/player/previous/<string:username>',
-                 view_func=player.PlayerPrevious.as_view(name='player_previous'),
-                 methods=['PUT'],
-                 endpoint='prev_username'
-                 )
-app.add_url_rule('/player/shuffle/<string:username>',
-                 view_func=player.PlayerShuffle.as_view(name='player_shuffle'),
-                 methods=['PUT'],
-                 endpoint='shuffle_username'
-                 )
-app.add_url_rule('/player/repeat/<string:username>',
-                 view_func=player.PlayerRepeat.as_view(name='player_repeat'),
-                 methods=['PUT'],
-                 endpoint='repeat_username'
-                 )
-app.add_url_rule('/player/play_pause',
-                 view_func=player.PlayerPlayPause.as_view(name='player_play_pause'),
-                 methods=['GET'],
-                 endpoint='play_pause'
-                 )
-app.add_url_rule('/player/next',
-                 view_func=player.PlayerNext.as_view(name='player_next'),
-                 methods=['GET'],
-                 endpoint='next'
-                 )
-app.add_url_rule('/player/previous',
-                 view_func=player.PlayerPrevious.as_view(name='player_previous'),
-                 methods=['GET'],
-                 endpoint='prev'
-                 )
-app.add_url_rule('/player/shuffle',
-                 view_func=player.PlayerShuffle.as_view(name='player_shuffle'),
-                 methods=['GET'],
-                 endpoint='shuffle'
-                 )
-app.add_url_rule('/player/repeat',
-                 view_func=player.PlayerRepeat.as_view(name='player_repeat'),
-                 methods=['GET'],
-                 endpoint='repeat'
-                 )
+# app.add_url_rule('/player/play_pause/<string:username>/<string:state>',
+#                  view_func=player.PlayerPlayPause.as_view(name='player_play_pause'),
+#                  methods=['PUT'],
+#                  endpoint='play_pause_username_state'
+#                  )
+# app.add_url_rule('/player/next/<string:username>',
+#                  view_func=player.PlayerNext.as_view(name='player_next'),
+#                  methods=['PUT'],
+#                  endpoint='next_username'
+#                  )
+# app.add_url_rule('/player/previous/<string:username>',
+#                  view_func=player.PlayerPrevious.as_view(name='player_previous'),
+#                  methods=['PUT'],
+#                  endpoint='prev_username'
+#                  )
+# app.add_url_rule('/player/shuffle/<string:username>',
+#                  view_func=player.PlayerShuffle.as_view(name='player_shuffle'),
+#                  methods=['PUT'],
+#                  endpoint='shuffle_username'
+#                  )
+# app.add_url_rule('/player/repeat/<string:username>',
+#                  view_func=player.PlayerRepeat.as_view(name='player_repeat'),
+#                  methods=['PUT'],
+#                  endpoint='repeat_username'
+#                  )
+# app.add_url_rule('/player/play_pause',
+#                  view_func=player.PlayerPlayPause.as_view(name='player_play_pause'),
+#                  methods=['GET'],
+#                  endpoint='play_pause'
+#                  )
+# app.add_url_rule('/player/next',
+#                  view_func=player.PlayerNext.as_view(name='player_next'),
+#                  methods=['GET'],
+#                  endpoint='next'
+#                  )
+# app.add_url_rule('/player/previous',
+#                  view_func=player.PlayerPrevious.as_view(name='player_previous'),
+#                  methods=['GET'],
+#                  endpoint='prev'
+#                  )
+# app.add_url_rule('/player/shuffle',
+#                  view_func=player.PlayerShuffle.as_view(name='player_shuffle'),
+#                  methods=['GET'],
+#                  endpoint='shuffle'
+#                  )
+# app.add_url_rule('/player/repeat',
+#                  view_func=player.PlayerRepeat.as_view(name='player_repeat'),
+#                  methods=['GET'],
+#                  endpoint='repeat'
+#                  )
