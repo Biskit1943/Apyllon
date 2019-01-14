c5087d800d7420048ee5c52b21a27c6d7a56abdc
Maximilian Konter <biskit1943@gmx.de>
2019-01-14 15:39:54 +0100

    Fix flask db upgrade. Switch to better logs. Somehow I get no logs since the fix of flask db upgrade but I must commit this patch

--- backend/player/queue.py
+++ backend/player/queue.py
@@ -3,12 +3,10 @@ import pafy
 
 class Queue():
     def __init__(self, identifier):
-        print("test")
         self.identifier = identifier
         self.media_list = vlc.MediaList()
 
     def add_local(self, filepath):
-        print("test")
         self.media_list.add_media(filepath)
 
     def add_youtube(self, url):
