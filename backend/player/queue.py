f00cdaf46a23bed3aba485c14e727c47b33c088d
Michael Schwab <michael.schwab@hs-augsburg.de>
2019-01-17 14:35:01 +0100

    Add read from database

--- backend/player/queue.py
+++ backend/player/queue.py
@@ -1,5 +1,6 @@
 import vlc
 import pafy
+import json
 
 class Queue():
     def __init__(self, identifier):
@@ -9,7 +10,23 @@ class Queue():
     def add_local(self, filepath):
         self.media_list.add_media(filepath)
 
+    def add_local_database_object(self, database_object):
+        try:
+            media = vlc.Media(database_object.filepath)
+            meta = vlc.Meta()
+            try:
+                meta.Artist = database_object.artist
+                meta.Title = database_object.title
+                meta.Album = database_object.album
+                meta.Genre = database_object.genre
+            except(Exception):
+                pass
+            self.media_list.add_media(media)
+        except(Exception):
+            pass
+
     def add_youtube(self, url):
         video = pafy.new(url)
         bestaudio = video.getbestaudio()
         self.media_list.add_media(bestaudio.url)
+
