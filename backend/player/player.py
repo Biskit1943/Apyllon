0dcc9335dbfa1e964bd336faeda19a30a8ccb94f
Michael Schwab <michael.schwab@hs-augsburg.de>
2019-01-17 17:54:03 +0100

    Fix player import

--- backend/player/player.py
+++ backend/player/player.py
@@ -2,7 +2,7 @@ import logging
 
 import vlc
 
-from queue import Queue
+from backend.player.queue import Queue
 
 logging = logging.getLogger("__main__")
 
@@ -72,7 +72,10 @@ class Player():
         self.player.play()
 
     def previous(self):
-        pass
+        if self.playing:
+            self.player.pause()
+        self.player.set_mrl(self.queue.get_previous_mrl())
+        self.player.play()
 
     def add_local(self, filepath):
         self.queue.add_local(filepath)
